e2badf029d559e4018a5eaa5026235cbfb2c468e
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2023-08-01 07:56:13 +0530

    changed title

--- src/dummyData.js
+++ src/dummyData.js
@@ -2,52 +2,52 @@ export const Users = [
   {
     id:1,
     profilePicture: "assets/person/1.jpeg",
-    username: "Safak Kocaoglu",
+    username: "ramesh",
   },
   {
     id:2,
     profilePicture: "assets/person/2.jpeg",
-    username: "Janell Shrum",
+    username: "suresh",
   },
   {
     id:3,
     profilePicture: "assets/person/3.jpeg",
-    username: "Alex Durden",
+    username: "dharmesh",
   },
   {
     id:4,
     profilePicture: "assets/person/4.jpeg",
-    username: "Dora Hawks",
+    username: "sandeep",
   },
   {
     id:5,
     profilePicture: "assets/person/5.jpeg",
-    username: "Thomas Holden",
+    username: "mandeep",
   },
   {
     id:6,
     profilePicture: "assets/person/6.jpeg",
-    username: "Shirley Beauchamp",
+    username: "Jay",
   },
   {
     id:7,
     profilePicture: "assets/person/7.jpeg",
-    username: "Travis Bennett",
+    username: "shivay",
   },
   {
     id:8,
     profilePicture: "assets/person/8.jpeg",
-    username: "Kristen Thomas",
+    username: "Om",
   },
   {
     id:9,
     profilePicture: "assets/person/9.jpeg",
-    username: "Gary Duty",
+    username: "Rudra",
   },
   {
     id:10,
     profilePicture: "assets/person/10.jpeg",
-    username: "Safak Kocaoglu",
+    username: "shiv",
   },
 ];
 
